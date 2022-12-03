d00139a5beeaa55a92225da5928454463ba2d137
Felipe Cechin <ficechin@gmail.com>
2022-12-03 16:09:45 -0300

    sweetalert file

---
+++ src/utils/sweetAlert.ts
@@ -0,0 +1,9 @@
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+export const reactSwal = withReactContent(Swal)
+
+export const sweetAlertOptions = {
+    confirmButtonColor: '#0e7490',
+    cancelButtonColor: '#d33',
+}
