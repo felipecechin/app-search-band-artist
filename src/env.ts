20985a36ba2b75468066ca9dc11afd67d1d27d34
Felipe Cechin <ficechin@gmail.com>
2022-12-03 16:07:28 -0300

    env file

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,8 @@
 const env = {
-    API: '',
+    YOUTUBE_API: String(process.env.NEXT_PUBLIC_YOUTUBE_API),
+    YOUTUBE_KEY: String(process.env.NEXT_PUBLIC_YOUTUBE_KEY),
+    TICKETMASTER_API: String(process.env.NEXT_PUBLIC_TICKETMASTER_API),
+    TICKETMASTER_KEY: String(process.env.NEXT_PUBLIC_TICKETMASTER_KEY),
 }
 
 export default env
