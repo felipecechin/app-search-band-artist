9b65850d9c778f68aba73ea62a0559c33106bc69
Felipe Cechin <ficechin@gmail.com>
2022-12-04 12:23:07 -0300

    added test environment

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const tsconfig = require('./tsconfig.json')
+const moduleNameMapper = require('tsconfig-paths-jest')(tsconfig)
+
+module.exports = {
+    testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+    setupFilesAfterEnv: [
+        '<rootDir>/src/tests/setupTests.ts'
+    ],
+    transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+    },
+    testEnvironment: 'jsdom',
+    moduleNameMapper
+} 
